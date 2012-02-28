f93e9b7c272facf419bcbfc7f64352f73de115c3
Alexey Borzov <avb@php.net>
2012-02-28 18:49:29 +0000

    Made it easier to customize client-side validation:  * All qf.Validator's methods are now public so can be called from overridden code  * CSS class names are now kept in config object
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_QuickForm2/trunk@323635 c90b9560-bf6c-de11-be94-00142212c4b1

--- data/js/quickform.js
+++ data/js/quickform.js
@@ -808,6 +808,16 @@ qf.Validator = function(form, rules)
     */
     this.errors = new qf.Map();
 
+   /**
+    * CSS classes to use when marking validation status
+    * @type {Object}
+    */
+    this.classes = {
+        error:    'error',
+        valid:    'valid',
+        ancestor: 'element'
+    };
+
     form.validator = this;
     qf.events.addListener(form, 'submit', qf.Validator.submitHandler);
 
@@ -879,231 +889,209 @@ qf.Validator.liveHandler = function (event)
     }
 };
 
-qf.Validator.prototype = (function() {
+qf.Validator.prototype = {
     /**
-     * Clears validation status and error message of a given element
-     *
-     * @param   {string} elementId
-     * @returns {Node}              Parent element that gets 'error' / 'valid'
-     *                              classes applied
-     * @private
+     * Called before starting the validation. May be used e.g. to clear the errors from form elements.
+     * @param {HTMLFormElement} form The form being validated currently
      */
-    function _clearValidationStatus(elementId)
+    onStart: function(form)
     {
-        var parent = _findParent(elementId);
-        qf.classes.remove(parent, ['error', 'valid']);
+        for (var i = 0, rule; rule = this.rules[i]; i++) {
+            this.removeRelatedErrors(rule);
+        }
+    },
 
-        var spans = parent.getElementsByTagName('span');
-        for (var i = spans.length - 1; i >= 0; i--) {
-            if (qf.classes.has(spans[i], 'error')) {
-                spans[i].parentNode.removeChild(spans[i]);
+    /**
+     * Called on setting the element error
+     *
+     * @param {string} elementId ID attribute of an element
+     * @param {string} errorMessage
+     */
+    onFieldError: function(elementId, errorMessage)
+    {
+        var parent = this.findAncestor(elementId);
+        qf.classes.add(parent, this.classes.error);
+
+        var error = document.createElement('span');
+        error.className = this.classes.error;
+        error.appendChild(document.createTextNode(errorMessage));
+        error.appendChild(document.createElement('br'));
+        if ('fieldset' != parent.nodeName.toLowerCase()) {
+            parent.insertBefore(error, parent.firstChild);
+        } else {
+            // error span should be inserted *after* legend, IE will render it before fieldset otherwise
+            var legends = parent.getElementsByTagName('legend');
+            if (0 == legends.length) {
+                parent.insertBefore(error, parent.firstChild);
+            } else {
+                legends[legends.length - 1].parentNode.insertBefore(error, legends[legends.length - 1].nextSibling);
+            }
+        }
+    },
+
+    /**
+     * Called on successfully validating the element
+     *
+     * @param {string} elementId
+     */
+    onFieldValid: function(elementId)
+    {
+        qf.classes.add(this.findAncestor(elementId), this.classes.valid);
+    },
+
+    /**
+     * Called on successfully validating the form
+     */
+    onFormValid: function() {},
+
+    /**
+     * Called on failed validation
+     */
+    onFormError: function() {},
+
+    /**
+     * Performs validation using the stored rules
+     * @param   {HTMLFormElement} form    The form being validated
+     * @returns {boolean}
+     */
+    run: function(form)
+    {
+        this.onStart(form);
+
+        this.errors.clear();
+        for (var i = 0, rule; rule = this.rules[i]; i++) {
+            if (this.errors.hasKey(rule.owner)) {
+                continue;
+            }
+            this.validate(rule);
+        }
+
+        if (this.errors.isEmpty()) {
+            this.onFormValid();
+            return true;
+
+        } else {
+            this.onFormError();
+            return false;
+        }
+    },
+
+    /**
+     * Performs live validation of an element and related ones
+     *
+     * @param {Event} event     Event triggering the validation
+     */
+    runLive: function(event)
+    {
+        var testId   = ' ' + event.target.id + ' ',
+            ruleHash = new qf.Map(),
+            length   = -1;
+
+        // first: find all rules "related" to the given element, clear their error messages
+        while (ruleHash.length() > length) {
+            length = ruleHash.length();
+            for (var i = 0, rule; rule = this.rules[i]; i++) {
+                if (!(rule instanceof qf.LiveRule) || ruleHash.hasKey(i)) {
+                    continue;
+                }
+                for (var j = 0, trigger; trigger = rule.triggers[j]; j++) {
+                    if (-1 < testId.indexOf(' ' + trigger + ' ')) {
+                        ruleHash.set(i, true);
+                        this.removeRelatedErrors(rule);
+                        testId += rule.triggers.join(' ') + ' ';
+                        break;
+                    }
+                }
             }
         }
 
-        return parent;
-    }
+        // second: run all "related" rules
+        for (i = 0; rule = this.rules[i]; i++) {
+            if (!ruleHash.hasKey(i) || this.errors.hasKey(rule.owner)) {
+                continue;
+            }
+            this.validate(rule);
+        }
+    },
 
     /**
-     * Returns the first ancestor of an element that is either a fieldset or has class "element"
+     * Performs validation, sets the element's error if validation fails.
+     *
+     * @param   {qf.Rule} rule Validation rule, maybe with chained rules
+     * @returns {boolean}
+     */
+    validate: function(rule)
+    {
+        var globalValid = false, localValid = rule.callback.call(this);
+
+        for (var i = 0, item; item = rule.chained[i]; i++) {
+            for (var j = 0, multiplier; multiplier = item[j]; j++) {
+                localValid = localValid && this.validate(multiplier);
+                if (!localValid) {
+                    break;
+                }
+            }
+            globalValid = globalValid || localValid;
+            if (globalValid) {
+                break;
+            }
+            localValid = true;
+        }
+
+        if (!globalValid && rule.message && !this.errors.hasKey(rule.owner)) {
+            this.errors.set(rule.owner, rule.message);
+            this.onFieldError(rule.owner, rule.message);
+        } else if (!this.errors.hasKey(rule.owner)) {
+            this.onFieldValid(rule.owner);
+        }
+
+        return globalValid;
+    },
+
+    /**
+     * Returns the first ancestor of an element that is either a fieldset, a form or has preset CSS class
      *
      * @param   {string} elementId
      * @returns {Node}
-     *
-     * @private
      */
-    function _findParent(elementId)
+    findAncestor: function(elementId)
     {
         var parent = document.getElementById(elementId);
-        while (!qf.classes.has(parent, 'element')
+        while (!qf.classes.has(parent, this.classes.ancestor)
                && 'fieldset' != parent.nodeName.toLowerCase()
+               && 'form' != parent.nodeName.toLowerCase()
         ) {
             parent = parent.parentNode;
         }
         return parent;
-    }
+    },
 
     /**
      * Removes error messages from owner element(s) of a given rule and chained rules
      *
-     * @param {qf.Map} errors
      * @param {qf.Rule} rule
-     * @private
      */
-    function _removeRelatedErrors(errors, rule)
+    removeRelatedErrors: function(rule)
     {
-        errors.remove(rule.owner);
-        _clearValidationStatus(rule.owner);
-        for (var i = 0, item; item = rule.chained[i]; i++) {
-            for (var j = 0, multiplier; multiplier = item[j]; j++) {
-                _removeRelatedErrors(errors, multiplier);
+        var i, j, item, multiplier,
+            parent = this.findAncestor(rule.owner);
+
+        this.errors.remove(rule.owner);
+        qf.classes.remove(parent, [this.classes.error, this.classes.valid]);
+
+        var spans = parent.getElementsByTagName('span');
+        for (i = spans.length - 1; i >= 0; i--) {
+            if (qf.classes.has(spans[i], this.classes.error)) {
+                spans[i].parentNode.removeChild(spans[i]);
+            }
+        }
+        for (i = 0; item = rule.chained[i]; i++) {
+            for (j = 0; multiplier = item[j]; j++) {
+                this.removeRelatedErrors(multiplier);
             }
         }
     }
-
-    return {
-        /**
-         * Called before starting the validation. May be used e.g. to clear the errors from form elements.
-         * @param {HTMLFormElement} form The form being validated currently
-         */
-        onStart: function(form)
-        {
-            for (var i = 0, rule; rule = this.rules[i]; i++) {
-                _removeRelatedErrors(this.errors, rule);
-            }
-        },
-
-        /**
-         * Called on setting the element error
-         *
-         * @param {string} elementId ID attribute of an element
-         * @param {string} errorMessage
-         */
-        onFieldError: function(elementId, errorMessage)
-        {
-            var parent = _findParent(elementId);
-            qf.classes.add(parent, 'error');
-
-            var error = document.createElement('span');
-            error.className = 'error';
-            error.appendChild(document.createTextNode(errorMessage));
-            error.appendChild(document.createElement('br'));
-            if ('fieldset' != parent.nodeName.toLowerCase()) {
-                parent.insertBefore(error, parent.firstChild);
-            } else {
-                // error span should be inserted *after* legend, IE will render it before fieldset otherwise
-                var legends = parent.getElementsByTagName('legend');
-                if (0 == legends.length) {
-                    parent.insertBefore(error, parent.firstChild);
-                } else {
-                    legends[legends.length - 1].parentNode.insertBefore(error, legends[legends.length - 1].nextSibling);
-                }
-            }
-        },
-
-        /**
-         * Called on successfully validating the element
-         *
-         * @param {string} elementId
-         */
-        onFieldValid: function(elementId)
-        {
-            qf.classes.add(_findParent(elementId), 'valid');
-        },
-
-        /**
-         * Called on successfully validating the form
-         */
-        onFormValid: function() {},
-
-        /**
-         * Called on failed validation
-         */
-        onFormError: function()
-        {
-            //alert(this.msgPrefix + '\n - ' + this.errors.getValues().join('\n - ') + '\n' + this.msgPostfix);
-        },
-
-        /**
-         * Performs validation using the stored rules
-         * @param   {HTMLFormElement} form    The form being validated
-         * @returns {boolean}
-         */
-        run: function(form)
-        {
-            this.onStart(form);
-
-            this.errors.clear();
-            for (var i = 0, rule; rule = this.rules[i]; i++) {
-                if (this.errors.hasKey(rule.owner)) {
-                    continue;
-                }
-                this.validate(rule);
-            }
-
-            if (this.errors.isEmpty()) {
-                this.onFormValid();
-                return true;
-
-            } else {
-                this.onFormError();
-                return false;
-            }
-        },
-
-        /**
-         * Performs live validation of an element and related ones
-         *
-         * @param {Event} event     Event triggering the validation
-         */
-        runLive: function(event)
-        {
-            var testId   = ' ' + event.target.id + ' ',
-                ruleHash = new qf.Map(),
-                length   = -1;
-
-            // first: find all rules "related" to the given element, clear their error messages
-            while (ruleHash.length() > length) {
-                length = ruleHash.length();
-                for (var i = 0, rule; rule = this.rules[i]; i++) {
-                    if (!(rule instanceof qf.LiveRule) || ruleHash.hasKey(i)) {
-                        continue;
-                    }
-                    for (var j = 0, trigger; trigger = rule.triggers[j]; j++) {
-                        if (-1 < testId.indexOf(' ' + trigger + ' ')) {
-                            ruleHash.set(i, true);
-                            _removeRelatedErrors(this.errors, rule);
-                            testId += rule.triggers.join(' ') + ' ';
-                            break;
-                        }
-                    }
-                }
-            }
-
-            // second: run all "related" rules
-            for (i = 0; rule = this.rules[i]; i++) {
-                if (!ruleHash.hasKey(i) || this.errors.hasKey(rule.owner)) {
-                    continue;
-                }
-                this.validate(rule);
-            }
-        },
-
-        /**
-         * Performs validation, sets the element's error if validation fails.
-         *
-         * @param   {qf.Rule} rule Validation rule, maybe with chained rules
-         * @returns {boolean}
-         */
-        validate: function(rule)
-        {
-            var globalValid = false, localValid = rule.callback.call(this);
-
-            for (var i = 0, item; item = rule.chained[i]; i++) {
-                for (var j = 0, multiplier; multiplier = item[j]; j++) {
-                    localValid = localValid && this.validate(multiplier);
-                    if (!localValid) {
-                        break;
-                    }
-                }
-                globalValid = globalValid || localValid;
-                if (globalValid) {
-                    break;
-                }
-                localValid = true;
-            }
-
-            if (!globalValid && rule.message && !this.errors.hasKey(rule.owner)) {
-                this.errors.set(rule.owner, rule.message);
-                this.onFieldError(rule.owner, rule.message);
-            } else if (!this.errors.hasKey(rule.owner)) {
-                this.onFieldValid(rule.owner);
-            }
-
-            return globalValid;
-        }
-    };
-})();
-
+};
 /**
  * @namespace Client-side implementations of Rules that are a bit too complex to inline
  */
