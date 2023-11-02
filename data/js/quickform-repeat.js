e85df5ecfbf79a68035b9573bdd8173798c04cb2
Alexey Borzov <avb@php.net>
2023-11-02 14:10:39 +0300

    Copyright year update

--- data/js/quickform-repeat.js
+++ data/js/quickform-repeat.js
@@ -3,7 +3,7 @@
  * Package version 2.3.1
  * https://pear.php.net/package/HTML_QuickForm2
  *
- * Copyright 2006-2022, Alexey Borzov, Bertrand Mansion
+ * Copyright 2006-2023, Alexey Borzov, Bertrand Mansion
  * Licensed under BSD 3-Clause License
  * https://opensource.org/licenses/BSD-3-Clause
  */
